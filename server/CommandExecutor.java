d314717249349c140183f5022de59146caf1701b
LauraMilenaRB <lauramilena.rbe@gmail.com>
2018-06-17 23:54:07 -0500

    Sockets y respuesta ok

--- server/CommandExecutor.java
+++ server/CommandExecutor.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 
 /* 
@@ -21,34 +22,39 @@ public class CommandExecutor {
 	static String run(String commandString)  throws Exception{
 		String result = "";
 		String line;
+		boolean error=false;
 		try {
 			// start the shell command running as a child processes
-			Process child = Runtime.getRuntime().exec("ghci");
-			child.waitFor();
-			OutputStream output2 = child.getOutputStream();
-			output2.write(commandString.getBytes());
-			/*output.write("x".getBytes());
-			Process child = Runtime.getRuntime().exec(commandString);*/
-
+            Process child = Runtime.getRuntime().exec("ghci");
+            BufferedWriter w = new BufferedWriter (new OutputStreamWriter (child.getOutputStream()));
+            w.write(commandString);
+            w.close();
 			// open a BufferedReader to read the output of the child process
 			BufferedReader output = new BufferedReader(new InputStreamReader(child.getInputStream()));
+            BufferedReader errorReader = new BufferedReader(new InputStreamReader(child.getErrorStream()));
 			// while the child process is still outputting, add the output to the result string
-			while ((line = output.readLine()) != null) {
-				result = result.concat(line);
+            while ((line = errorReader.readLine()) != null) {
+                System.out.println(line);
+                result = result.concat(line);
+                result = result.concat("\n");
+                error=true;
+            }
+            errorReader.close();
+			while ((line = output.readLine()) != null && !error) {
+                System.out.println(line);
+                result = result.concat(line);
 				result = result.concat("\n");
 			}
-
 			result = result.concat("\n");
 			// add "END_MESSAGE" to the result string. When the client sees END_MESSAGE it
 			// will know that the server is done sending
 			result = result.concat("END_MESSAGE");
 			output.close();
+            child.destroy();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		return result;
 	}
-
 }
